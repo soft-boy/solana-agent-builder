2b94a58784ee1eac401b636076fab1e37f87ff1d
Eric Wang <ewang1997@gmail.com>
2025-01-17 10:26:12 -0800

    remove swagger

--- app/api/test.ts
+++
@@ -1,8 +0,0 @@
-export const dynamic = 'force-dynamic'; // static by default, unless reading the request
-
-export function GET(request: Request) {
-  const url = new URL(request.url);
-  const name = url.searchParams.get('name') || 'world';
-  
-  return new Response(`Hello ${name}`);
-}
