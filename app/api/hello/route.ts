70c0ac44aa305680afc5cb48965a4c5a9a27f8dd
Eric Wang <ewang1997@gmail.com>
2025-01-17 08:34:19 -0800

    create docs.ts file

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,5 +1,8 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
- 
+
 export function GET(request: Request) {
-  return new Response(`Hello world`);
+  const url = new URL(request.url);
+  const name = url.searchParams.get('name') || 'world';
+  
+  return new Response(`Hello ${name}`);
 }
